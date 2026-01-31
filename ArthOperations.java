15439cdc6d781a64866fac36264d63ae77685219
PURNACHANDRARAO <pondurupurnachandrarao@gmail.com>
2026-01-31 17:05:08 +0530

    Add files via upload

---
+++ ArthOperations.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class ArthOperations
+{
+	public static void main(String args[])
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.print("enter a and b values: ");
+	int a=sc.nextInt();
+	double b=sc.nextDouble();
+
+
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+}
+}
+
+
+
+
+
+
+
