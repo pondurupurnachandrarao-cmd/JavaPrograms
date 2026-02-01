4cce79677a201e66445e71cfaa1e6d10a0c62e5b
PURNACHANDRARAO <pondurupurnachandrarao@gmail.com>
2026-02-01 09:22:32 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,24 @@
+class DataTypes
+ {
+    public static void main(String args[])
+ {
+
+        byte b = 10;
+        short s = 200;
+        int i = 5000;
+        long l = 100000L;
+        float f = 5.5f;
+        double d = 123.456;
+        char c = 'A';
+        boolean bl = true;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bl);
+    }
+}
