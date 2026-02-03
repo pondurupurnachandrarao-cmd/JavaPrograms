8ff2fed63ed1bff971d4a41abd0ced3ebe95c599
PURNACHANDRARAO <pondurupurnachandrarao@gmail.com>
2026-02-03 08:29:59 +0530

    Add files via upload

---
+++ WelcomeUser.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class WelcomeUser
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter your  name: ");
+String name=sc.next();
+System.out.println("Welcome "+name+"!");
+}
+}
