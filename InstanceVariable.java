bcc6cd4a50d472c2a20eae2411a7209921da1576
PURNACHANDRARAO <pondurupurnachandrarao@gmail.com>
2026-02-18 21:22:01 +0530

    Add files via upload

---
+++ InstanceVariable.java
@@ -0,0 +1,9 @@
+class InstanceVariable
+{
+int a=987;
+public static void main(String args[])
+{
+InstanceVariable obj=new InstanceVariable();
+System.out.println("Instance variable a value is: "+obj.a);
+}
+}
