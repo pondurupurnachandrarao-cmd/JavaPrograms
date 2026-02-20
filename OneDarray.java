5b380818ac5843b8ab7b7178cb3d1aa12c710548
PURNACHANDRARAO <pondurupurnachandrarao@gmail.com>
2026-02-20 21:13:30 +0530

    Add files via upload

---
+++ OneDarray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class OneDarray
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a[]=new int[5];
+System.out.println("enter array elements:");
+for(int i=0;i<a.length;i++)
+{
+a[i]=sc.nextInt();
+
+}
+System.out.println("array elements are:");
+for(int i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
