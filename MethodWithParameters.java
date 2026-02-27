38aee4bdd9e19b20234a5f0b0875e64b41ed5620
PURNACHANDRARAO <pondurupurnachandrarao@gmail.com>
2026-02-27 23:22:09 +0530

    Add files via upload

---
+++ MethodWithParameters.java
@@ -0,0 +1,13 @@
+class MethodWithParameters
+{
+    void display(String name)
+    {
+        System.out.println("Name: " + name);
+    }
+
+    public static void main(String args[])
+    {
+        MethodWithParameters obj = new MethodWithParameters();
+        obj.display("Arjun");
+    }
+}
